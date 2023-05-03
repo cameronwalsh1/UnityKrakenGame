54f10081b44f64991f9b12531af45075f5f5d0bd
cameronwalsh1 <cameron_walsh1@hotmail.com>
2023-05-03 05:28:57 -0500

    Allow Key Selection for Scripts
    Modified the Level Controller script, allowing user to change the key used to activate the flashes.

--- Assets/ScreenFlashAssets/LevelControllerScreenFlash.cs
+++ Assets/ScreenFlashAssets/LevelControllerScreenFlash.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class LevelControllerScreenFlash : MonoBehaviour
 {
+    [Header("Chosen Flash")]
     [SerializeField] ScreenFlashImage _flashImage = null;
+    [Header("Assign Button To Activate Flash")]
+    [SerializeField] string key = "space";
+    [Header("Flash Variables")]
     [SerializeField] Color _newColor = Color.red;
-    [SerializeField] float _timeLength = 1;
+    [SerializeField] float _timeLength = 1f;
     [SerializeField] float _opacity = .5f;
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(key))
         {
             _flashImage.BeginFlash(_timeLength, _opacity, _newColor);
         }
